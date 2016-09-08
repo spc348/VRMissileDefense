19cb181f272e71dcdc47b5bbca644865fdb239a7
Sam Seidenberg <seidensj@gmail.com>
2016-09-07 23:21:48 -0400

    lock on!

--- Assets/Scripts/LockOnReticle.cs
+++ Assets/Scripts/LockOnReticle.cs
@@ -11,6 +11,7 @@ public class LockOnReticle : MonoBehaviour
 	//this is the ui element
 	public RectTransform UI_Element;
 	public Camera camera;
+
 	void Start ()
 	{
 		//first you need the RectTransform component of your canvas
@@ -29,10 +30,12 @@ public class LockOnReticle : MonoBehaviour
 //
 		Vector2 ViewportPosition = camera.WorldToViewportPoint (WorldObject.transform.position);
 		Vector2 WorldObject_ScreenPosition = new Vector2 (
-			                                    ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
-			                                    ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
-		
+			                                     ((ViewportPosition.x * CanvasRect.sizeDelta.x * 2f) - (CanvasRect.sizeDelta.x )),
+			                                     ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+//		WorldObject_ScreenPosition.x = -WorldObject_ScreenPosition.x;
 		//now you can set the position of the ui element
+		print ("pos: " + WorldObject_ScreenPosition);
 		UI_Element.anchoredPosition = WorldObject_ScreenPosition;
 
 		float dot = Vector3.Dot ((WorldObject.transform.position - CanvasRect.transform.position).normalized, Camera.main.transform.forward);

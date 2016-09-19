3d4dd41c22994764e5b1b3dd0f120a7d0ecf699b
Sam <seidensj@gmail.com>
2016-09-19 16:46:47 -0400

    fence

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : InteractableObject {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

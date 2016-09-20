5fd0c35852a37ab90e7a9b08dde9a33eecec1558
Sam <seidensj@gmail.com>
2016-09-20 17:44:05 -0400

    shooter alien

--- Assets/Scripts/AirShooterEnemy.cs
+++ Assets/Scripts/AirShooterEnemy.cs
@@ -6,15 +6,22 @@ public class AirShooterEnemy : Enemy
 
 	[SerializeField] private ObjectPoolerScript _projectilePooler;
 	[SerializeField] private GameObject bulletSpawnPos;
+	[SerializeField] private GameObject _eyeLeft;
+	[SerializeField] private GameObject _eyeRight;
+	[SerializeField] private GameObject _mouthTop;
+	[SerializeField] private GameObject _mouthBottom;
+	public float _fireRate = .25f;
 	[SerializeField] private float _stopDistanceFromTarget = 200f;
 	private float _nextFireTime;
-	public float _fireRate = .25f;
-
+	[SerializeField] private GameObject _mouthTopOrigPos;
+	[SerializeField] private GameObject _mouthBottomOrigPos;
+	 
 	[SerializeField] private GameObject[] _tenactles;
 
 	// Use this for initialization
 	void Start ()
 	{
+		
 		_target = TargetManager.Instance.getTarget ();
 	}
 	
@@ -23,6 +30,7 @@ public class AirShooterEnemy : Enemy
 	{
 		move ();
 		attack ();
+		eyesLookAtTarget ();
 	}
 
 	public override void move ()
@@ -47,14 +55,47 @@ public class AirShooterEnemy : Enemy
 		if (_target != null) {
 			if (Time.time > _nextFireTime) {
 				_nextFireTime = Time.time + _fireRate;
-				
+//				StartCoroutine (openMouthCoroutine ());
 				GameObject bullet = _projectilePooler.GetPooledObject ();
 				bullet.transform.position = transform.position;
-				Vector3 dir = _target.transform.position - bulletSpawnPos.transform.position;
+				Vector3 dir = _target.transform.position - bullet.transform.position;
 				bullet.SetActive (true);
+				bullet.GetComponent<Rigidbody> ().velocity = Vector3.zero;
 				bullet.GetComponent<Rigidbody> ().AddForce (dir * 1, ForceMode.Impulse); 
 			}	
 		}
+	}
+
+	void eyesLookAtTarget ()
+	{
+		if (_target != null) {
+			_eyeLeft.transform.LookAt (_target.transform);
+			_eyeRight.transform.LookAt (_target.transform);
+//			float r = Random.Range (-10f, 10f);
+//			_eyeRight.GetComponent<Rigidbody> ().AddTorque (Vector3.one * r); 
+//				.LookAt (_target.transform);
+		}
+	}
+
+	IEnumerator openMouthCoroutine ()
+	{
+		openMouth ();
+		yield return new WaitForSeconds (1.1f);
+		closeMouth ();
+	}
+
+	void openMouth ()
+	{
+		float amount = .25f;
+		LeanTween.moveLocalY (_mouthTop.gameObject, amount, 1f).setEase (LeanTweenType.easeOutExpo);
+		LeanTween.moveLocalY (_mouthBottom.gameObject, -amount, 1f).setEase (LeanTweenType.easeOutExpo);
+	}
+
+	void closeMouth ()
+	{
+		float amount = 1f;
+		LeanTween.move (_mouthTop.gameObject, _mouthTopOrigPos.transform.position, .25f).setEase (LeanTweenType.easeOutExpo);
+		LeanTween.move (_mouthBottom.gameObject, _mouthBottomOrigPos.transform.position, .25f).setEase (LeanTweenType.easeOutExpo);
 	}
 
 
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,33 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
 
 	[SerializeField] private GameObject _bulletExplosion;
-
+	[SerializeField] Rigidbody _rb;
+	[SerializeField] Renderer _renderer;
+	[SerializeField] Light _light;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		StartCoroutine (dieAfterTime ());
 	}
-	
 
+	void OnEnable ()
+	{
+		_renderer.enabled = true;
+		_light.enabled = true;
+	}
 
 	protected void OnCollisionEnter (Collision coll)
 	{
-		GameObject bulletExplosion = Instantiate(_bulletExplosion, coll.transform.position, Quaternion.identity) as GameObject;
-		gameObject.SetActive(false);
+		StartCoroutine (dieCoroutine (coll));
+//		GameObject bulletExplosion = Instantiate (_bulletExplosion, coll.transform.position, Quaternion.identity) as GameObject;
+//		gameObject.SetActive (false);
 		if (coll.gameObject.CompareTag ("Target")) {
 			coll.gameObject.GetComponent<Target> ().takeDamage (1);
 		}
 
 	}
 
-	IEnumerator dieAfterTime() 
+	IEnumerator dieCoroutine(Collision coll) {
+		_rb.velocity = Vector3.zero;
+		_light.enabled = false;
+		GameObject bulletExplosion = Instantiate (_bulletExplosion, coll.transform.position, Quaternion.identity) as GameObject;
+
+		float trailLiveTime = .21f;
+		yield return new WaitForSeconds (trailLiveTime);
+		gameObject.SetActive (false);
+	}
+
+	IEnumerator dieAfterTime ()
 	{
 		if (gameObject.activeInHierarchy) {
 			yield return new WaitForSeconds (10f);
-			GameObject bulletExplosion = Instantiate(_bulletExplosion, transform.position, Quaternion.identity) as GameObject;
-			gameObject.SetActive(false);
+			_rb.velocity = Vector3.zero;
+			_light.enabled = false;
+			GameObject bulletExplosion = Instantiate (_bulletExplosion, transform.position, Quaternion.identity) as GameObject;
+
+			float trailLiveTime = .21f;
+			yield return new WaitForSeconds (trailLiveTime);
+			gameObject.SetActive (false);
 		}
 	}
 }
